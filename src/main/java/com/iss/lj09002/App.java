ae4b1cc97d05780ab265dd5e26ee22c328428196
Maynor Gabriel López Jiménez <maynorlop3z@gmail.com>
2024-05-03 19:53:48 -0600

    primer commit

---
+++ src/main/java/com/iss/lj09002/App.java
@@ -0,0 +1,17 @@
+package com.iss.lj09002;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String Hello() {
+		return "Hello Class";
+	}
+    public static void main( String[] args )
+    {
+    	App hw = new App();
+        System.out.println( hw.Hello() );
+    }
+}
